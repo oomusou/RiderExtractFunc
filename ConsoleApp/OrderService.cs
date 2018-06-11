4534bd095ad2db7e895c5d801a5f61eb385d78ae
oomusou <oomusou@gmail.com>
2018-06-11 20:41:23 +0800

    Before Refactoring

---
+++ ConsoleApp/OrderService.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp
+{
+    public class OrderService
+    {
+        public double CalculatePrice()
+        {
+            var price = 1000.0;
+            return price * 0.80;
+        }
+    }
+}
